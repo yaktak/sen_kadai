f01da4385855ef545e37814497d72307e3ce2c47
yaktak <yakei034@gmail.com>
2016-11-12 15:42:07 +0900

    make hello

---
+++ tmp/324b38cd6557c2aa938c57b0b6da7652^%%C9^C9D^C9D5DCE5%%hello.tpl.php
@@ -0,0 +1,4 @@
+<?php /* Smarty version 2.6.30, created on 2016-11-12 15:35:52
+         compiled from hello.tpl */ ?>
+Hello Ethnam<br />
+current time: <?php echo $this->_tpl_vars['app']['now']; ?>
