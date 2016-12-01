e40523f3eadb7b3dfd4141e98826e792ed4b9297
yaktak <yakei034@gmail.com>
2016-12-01 18:08:56 +0900

    commit

--- tmp/324b38cd6557c2aa938c57b0b6da7652^%%C9^C9D^C9D5DCE5%%hello.tpl.php
+++ tmp/324b38cd6557c2aa938c57b0b6da7652^%%C9^C9D^C9D5DCE5%%hello.tpl.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version 2.6.30, created on 2016-11-12 15:35:52
+<?php /* Smarty version 2.6.30, created on 2016-12-01 18:04:29
          compiled from hello.tpl */ ?>
 Hello Ethnam<br />
 current time: <?php echo $this->_tpl_vars['app']['now']; ?>
