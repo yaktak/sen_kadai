184cb35bb1ccc5e8c08c05a236f7139d1697c0ef
yaktak <yakei034@gmail.com>
2016-12-01 18:37:08 +0900

    2-login-process-finished

---
+++ app/view/Login.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ *  Login.php
+ *
+ *  @author     {$author}
+ *  @package    Testapp
+ */
+
+/**
+ *  login view implementation.
+ *
+ *  @author     {$author}
+ *  @access     public
+ *  @package    Testapp
+ */
+class Testapp_View_Login extends Testapp_ViewClass
+{
+    /**
+     *  preprocess before forwarding.
+     *
+     *  @access public
+     */
+    public function preforward()
+    {
+    }
+}
+
